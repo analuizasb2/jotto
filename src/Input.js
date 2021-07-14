26e736f691854f7fd3e5ca78ae327f06c9d88612
laanaluiza.braga <laanaluiza.braga@laanaluizabragas-MacBook-Pro.local>
2021-07-14 19:07:46 -0300

    added input proptypes testing

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-Input.propTypes = {};
-
-function Input(props) {
+const Input = ({ secretWord }) => {
   return <div data-test="component-input"></div>;
-}
+};
+
+Input.propTypes = { secretWord: PropTypes.string.isRequired };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,13 +1,21 @@
 import Input from "./Input";
-import { findByTestAttribute } from "../test/testUtils";
+import { checkProps, findByTestAttribute } from "../test/testUtils";
 import { shallow } from "enzyme";
 
-const setup = () => {
-  return shallow(<Input />);
+const defaultProps = { secretWord: "party" };
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ secretWord: true });
   const componentInput = findByTestAttribute(wrapper, "component-input");
   expect(componentInput.length).toBe(1);
+});
+
+test("does not throw warning when prop types are correct", () => {
+  const props = { secretWord: "true" };
+  checkProps(Input, props);
 });
